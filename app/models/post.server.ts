ae74ccc89c3dae228738ccc2468ebbd507a6ee4c
vivek lokhande <vivek2811999@gmail.com>
2023-02-17 11:56:58 +0530

    feat:added fn to updateuser from DB

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,6 +1,8 @@
 import { Post } from "@prisma/client";
 import { prisma } from "~/db.server";
 
+export type { Post };
+
 export async function getPostsListings() {
   return prisma.post.findMany({
     select: {
@@ -38,4 +40,15 @@ export async function createPost(
     /*Pick refer(https://ultimatecourses.com/blog/using-typescript-pick-mapped-type) */
   }
   return prisma.post.create({ data: post });
+}
+
+export async function updatePost(
+  slug: string,
+  post: Pick<Post, "slug" | "title" | "markdown">
+) {
+  return prisma.post.update({ data: post, where: { slug } });
+}
+
+export async function deletePost(slug: string) {
+  return prisma.post.delete({ where: { slug } });
 }
